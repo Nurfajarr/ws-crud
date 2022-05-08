3459c0e6c33dfdcc8febc2a539897751da90a87c
Nurfajar <fnur1815@gmail.com>
2022-05-08 21:31:00 +0700

    update controller

--- src/main/java/net/guides/springboot2/crud/controller/EmployeeController.java
+++ src/main/java/net/guides/springboot2/crud/controller/EmployeeController.java
@@ -8,14 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import net.guides.springboot2.crud.exception.ResourceNotFoundException;
 import net.guides.springboot2.crud.model.Employee;
@@ -27,39 +20,47 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
-	@GetMapping("/employees")
+	@GetMapping("/get_all_employees")
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
 
-	@GetMapping("/employees/{id}")
-	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+	@GetMapping("/get_employees")
+	public ResponseEntity<Employee> getEmployeeById(@RequestParam(value = "id") Long employeeId)
 			throws ResourceNotFoundException {
 		Employee employee = employeeRepository.findById(employeeId)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
 		return ResponseEntity.ok().body(employee);
 	}
 
-	@PostMapping("/employees")
-	public Employee createEmployee(@Valid @RequestBody Employee employee) {
+	@PostMapping("/save_employees")
+	public Employee createEmployee(@RequestParam(value = "first_name", required = false) String firstname,
+								   @RequestParam(value = "last_name", required = false) String lastname,
+								   @RequestParam(value = "email_id", required = false) String emailId) {
+		Employee employee = new Employee();
+		employee.setFirstName(firstname);
+		employee.setLastName(lastname);
+		employee.setEmailId(emailId);
 		return employeeRepository.save(employee);
 	}
 
-	@PutMapping("/employees/{id}")
-	public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
-			@Valid @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+	@PutMapping("/edit_employees")
+	public ResponseEntity<Employee> updateEmployee(@RequestParam(value = "id") Long employeeId,
+												   @RequestParam(value = "first_name", required = false) String firstname,
+												   @RequestParam(value = "last_name", required = false) String lastname,
+												   @RequestParam(value = "email_id", required = false) String emailId) throws ResourceNotFoundException {
 		Employee employee = employeeRepository.findById(employeeId)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
 
-		employee.setEmailId(employeeDetails.getEmailId());
-		employee.setLastName(employeeDetails.getLastName());
-		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setEmailId(emailId);
+		employee.setLastName(lastname);
+		employee.setFirstName(firstname);
 		final Employee updatedEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updatedEmployee);
 	}
 
-	@DeleteMapping("/employees/{id}")
-	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+	@DeleteMapping("/delete_employees")
+	public Map<String, Boolean> deleteEmployee(@RequestParam(value = "id") Long employeeId)
 			throws ResourceNotFoundException {
 		Employee employee = employeeRepository.findById(employeeId)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
--- src/main/java/net/guides/springboot2/crud/model/Employee.java
+++ src/main/java/net/guides/springboot2/crud/model/Employee.java
@@ -6,10 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "employees")
-public class Employee {
+public class Employee implements Serializable {
 
 	private long id;
 	private String firstName;
